f25673717d521bf363a0ab1d87d66d9db3191065
Gabriel Brañeiro <gabbraneiro@gmail.com>
2020-05-12 18:23:48 -0300

    Base

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { RadioPlayerService } from '../radio/radio-player.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+    providers: [RadioPlayerService]
 })
 export class HomePage {
+    isPlaying: boolean = false;
+    isMute: boolean = false;
 
-  constructor() {}
+    constructor(public player: RadioPlayerService) { }
 
+    play() {
+        this.player.play().then(() => {
+            this.isPlaying = true;
+        });
+    }
+
+    pause() {
+        this.player.pause();
+        this.isPlaying = false;
+    }
+
+    mute() {
+        this.player.mute();
+        this.isMute = true;
+    }
+
+    unmute() {
+        this.player.unmute();
+        this.isMute = false;
+    }
+
+    goTo(redSocial) {
+        switch (redSocial) {
+            case 'facebook':
+                window.location.href = environment.urlFacebook;
+                break;
+            case 'instagram':
+                window.location.href = environment.urlInstagram;
+                break;
+            case 'whatsapp':
+                window.location.href = environment.urlWhatsapp;
+                break
+            case 'twitter':
+                window.location.href = environment.urlTwitter;
+                break
+            default:
+                break;
+        }
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    urlRadio: "http://radio02.ferozo.com:8042/;stream/1",
+    urlFacebook: "https://facebook.com/RCH90.9/",
+    urlInstagram: "https://instagram.com/marcaregistrada.2019/?igshid=ov0r1tdjjabu",
+    urlWhatsapp: "https://api.whatsapp.com/send?phone=5492241683434",
+    urlTwitter: "https://twitter.com/rch909?s=08",
 };
 
 /*
